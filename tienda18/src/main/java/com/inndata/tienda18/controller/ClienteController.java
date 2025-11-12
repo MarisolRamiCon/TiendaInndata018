1b52f22879856cb331a3049208c1f3046ba005bd
evvanyoropeza <evvanyoropeza@gmail.com>
2025-11-11 20:50:05 -0600

    Subiendo mi rama desde IntelliJ

---
+++ tienda18/src/main/java/com/inndata/tienda18/controller/ClienteController.java
@@ -0,0 +1,21 @@
+package com.inndata.tienda18.controller;
+
+import com.inndata.tienda18.entity.Clientes;
+import com.inndata.tienda18.service.impl.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ClienteController {
+    @Autowired
+    ClienteService clienteService;
+    @GetMapping("/clientes")
+    public List<Clientes> readAll(){
+        return clienteService.readAll();
+    }
+}
